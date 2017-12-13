daf5fb0daf10407dd29c8565c7cb434eb0fd6899
KaoTD <kaotd99@gmail.com>
2017-12-13 17:25:13 +0000

    small fixes

--- aosp_ef59.mk
+++ aosp_ef59.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from ef59 device
 $(call inherit-product, device/pantech/ef59/ef59.mk)
